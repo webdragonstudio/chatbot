743e690805904f670b090d7011d269545144ce71
Darwin Nogueira <73005368+webdragonstudio@users.noreply.github.com>
2025-08-06 09:06:48 -0300

    chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -25,7 +25,7 @@ client.on('message', async msg => {
 
         await delay(3000); //delay de 3 segundos
         await chat.sendStateTyping(); // Simulando Digitação
-        await delay(3000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
+        await delay(3000); //Delay de 3 segundos
         const contact = await msg.getContact(); //Pegando o contato
         const name = contact.pushname; //Pegando o nome do contato
         await client.sendMessage(msg.from,'Olá! '+ name.split(" ")[0] + 'Sou o assistente virtual da empresa tal. Como posso ajudá-lo hoje? Por favor, digite uma das opções abaixo:\n\n1 - Como funciona\n2 - Valores dos planos\n3 - Benefícios\n4 - Como aderir\n5 - Outras perguntas'); //Primeira mensagem de texto
@@ -65,7 +65,7 @@ client.on('message', async msg => {
         const chat = await msg.getChat();
 
 
-        await delay(3000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
+        await delay(3000); //Delay de 3 segundos
         await chat.sendStateTyping(); // Simulando Digitação
         await delay(3000);
         await client.sendMessage(msg.from, '*Plano Individual:* R$22,50 por mês.\n\n*Plano Família:* R$39,90 por mês, inclui você mais 3 dependentes.\n\n*Plano TOP Individual:* R$42,50 por mês, com benefícios adicionais como\n\n*Plano TOP Família:* R$79,90 por mês, inclui você mais 3 dependentes');
@@ -80,7 +80,7 @@ client.on('message', async msg => {
         const chat = await msg.getChat();
 
 
-        await delay(3000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
+        await delay(3000); //Delay de 3 segundos
         await chat.sendStateTyping(); // Simulando Digitação
         await delay(3000);
         await client.sendMessage(msg.from, 'Sorteio de em prêmios todo ano.\n\nAtendimento médico ilimitado 24h por dia.\n\nReceitas de medicamentos');
@@ -95,7 +95,7 @@ client.on('message', async msg => {
     if (msg.body !== null && msg.body === '4' && msg.from.endsWith('@c.us')) {
         const chat = await msg.getChat();
 
-        await delay(3000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
+        await delay(3000); //Delay de 3 segundos
         await chat.sendStateTyping(); // Simulando Digitação
         await delay(3000);
         await client.sendMessage(msg.from, 'Você pode aderir aos nossos planos diretamente pelo nosso site ou pelo WhatsApp.\n\nApós a adesão, você terá acesso imediato');
@@ -112,13 +112,14 @@ client.on('message', async msg => {
     if (msg.body !== null && msg.body === '5' && msg.from.endsWith('@c.us')) {
         const chat = await msg.getChat();
 
-        await delay(3000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
+        await delay(3000); //Delay de 3 segundos
         await chat.sendStateTyping(); // Simulando Digitação
         await delay(3000);
         await client.sendMessage(msg.from, 'Se você tiver outras dúvidas ou precisar de mais informações, por favor, fale aqui nesse whatsapp ou visite nosso site: https://site.com ');
 
 
     }
+
 
 
 
